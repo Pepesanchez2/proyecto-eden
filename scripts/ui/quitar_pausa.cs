6518b4e44f1701952de4245f37361b8d6222e74c
Marc Sanchez <txukyjunior@gmail.com>
2026-02-10 17:13:16 +0100

    Boton pausa

---
+++ scripts/ui/quitar_pausa.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class quitar_pausa : Button
+{
+    public override void _Ready()
+    {
+        Pressed += () =>
+        {
+            GetParent<Control>().GetParent<menu_pausa>().QuitarPausa();
+        };
+    }
+}
