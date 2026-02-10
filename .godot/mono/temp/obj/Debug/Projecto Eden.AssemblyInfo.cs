1a19633651aa9381e90c1dc55ac772565a40c366
David Rocer <drocher951@gmail.com>
2026-02-10 16:59:32 +0100

    Por si

--- .godot/mono/temp/obj/Debug/Projecto Eden.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Projecto Eden.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projecto Eden")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80062bea4f4ec1a87a230f4d7be06cbe7b5a9080")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projecto Eden")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projecto Eden")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
