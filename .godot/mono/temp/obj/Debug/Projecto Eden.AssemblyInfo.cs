0be185930591522bd84a6f622a9e2d2b00bb7f86
David Rocer <drocher951@gmail.com>
2026-02-10 16:49:15 +0100

    Escenas de balas

--- .godot/mono/temp/obj/Debug/Projecto Eden.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Projecto Eden.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projecto Eden")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dbf8c3e6c68040e3367bed0f772efba42102477")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+221600eb6836e497aaed2358f7644bad281308ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projecto Eden")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projecto Eden")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
